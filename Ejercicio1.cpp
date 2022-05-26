ffc3f3adec0e7d94a0f29699197b444813016015
Otoniel2514 <00045221@uca.edu.sv>
2022-05-26 14:18:00 -0700

    Add: EjercicioTutoria1

---
+++ Ejercicio1.cpp
@@ -0,0 +1,27 @@
+//Programa para ingresar datos usando los tipos de variables, int,string,char,double y float
+
+#include <iostream>
+using namespace std;
+int main(){
+    int number; //Variable de tipo numero entero 
+    string word; //Variable de tipo palabra
+    char h; // Variable de tipo palabra 
+    double numbers; //Variable de tipo decimal
+    float n; //Variable de tipo flotante(decimal)
+
+    //Pedirle los datos al usuario
+    cout<<endl;
+    cout<<"Ingrese un numero entero: ";
+    cin>>number;
+    cout<<"Ingrese una palabra: ";
+    cin>>word;
+    cout<<"Ingrese un caracter: ";
+    cin>>h;
+    cout<<"Ingrese un numero decimal: ";
+    cin>>numbers;
+    cout<<"Ingrese un numero decimal: ";
+    cin>>n;
+
+    return 0;
+
+};
